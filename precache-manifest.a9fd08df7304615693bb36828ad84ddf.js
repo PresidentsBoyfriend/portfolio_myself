054f444fb81c6580bb1822fc2e8526d2312c947a
PresidentsBoyfriend <fs.as.8585@gmail.com>
2019-09-07 20:40:57 +0300

    Updates

---
+++ precache-manifest.a9fd08df7304615693bb36828ad84ddf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e2a80b3d133d47c340b803c3ec3dd9",
+    "url": "/portfolio_myself/index.html"
+  },
+  {
+    "revision": "0d7cefbbc2121a5a9273",
+    "url": "/portfolio_myself/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "3a29caa95b7bd36f3f72",
+    "url": "/portfolio_myself/static/css/main.70da1edf.chunk.css"
+  },
+  {
+    "revision": "0d7cefbbc2121a5a9273",
+    "url": "/portfolio_myself/static/js/2.a0a69c00.chunk.js"
+  },
+  {
+    "revision": "3a29caa95b7bd36f3f72",
+    "url": "/portfolio_myself/static/js/main.72b364be.chunk.js"
+  },
+  {
+    "revision": "6739e7bcf6e7001eb4ca",
+    "url": "/portfolio_myself/static/js/runtime~main.d2ad3b10.js"
+  },
+  {
+    "revision": "89bdb6856b3e37bfe5205f13e1dd6662",
+    "url": "/portfolio_myself/static/media/architecture.89bdb685.jpg"
+  },
+  {
+    "revision": "608434509719cbe0e52ed5e07f9d2db6",
+    "url": "/portfolio_myself/static/media/automotive.60843450.jpg"
+  },
+  {
+    "revision": "a56ed52c8f74c0072f1ab48d221b482b",
+    "url": "/portfolio_myself/static/media/nature.a56ed52c.jpg"
+  },
+  {
+    "revision": "7a289160cea05b236dbd13d942d471ae",
+    "url": "/portfolio_myself/static/media/people.7a289160.jpg"
+  }
+]);
